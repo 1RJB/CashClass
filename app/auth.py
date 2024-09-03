378e2ecd560b1b8dfd0367e80a2537206c486046
Ryan <ryanb0717@gmail.com>
2024-09-03 15:44:32 +0800

    chore: Update route paths for lesson pages

--- app/auth.py
+++ app/auth.py
@@ -161,27 +161,28 @@ def parse_quiz_data(quiz_text):
         return []
 
 @auth.route('/quiz')
+@login_required
 def quiz():
     quiz = get_quiz()
     return render_template('quiz.html', quiz=quiz)
 
-@auth.route('/lesson_home.html')
+@auth.route('/lesson_home')
 def lesson_main():
-    return render_template('lesson_home.html', lesson_main=lesson_main )
+    return render_template('lesson_home', lesson_main=lesson_main )
 
-@auth.route('/lesson1.html')
+@auth.route('/lesson1')
 def lesson1():
     return render_template('lesson1.html', lesson1=lesson1)
 
-@auth.route('/lesson2.html')
+@auth.route('/lesson2')
 def lesson2():
     return render_template('lesson2.html', lesson2=lesson2)
 
-@auth.route('/lesson3.html')
+@auth.route('/lesson3')
 def lesson3():
     return render_template('lesson3.html', lesson3=lesson3)
 
-@auth.route('/lesson4.html')
+@auth.route('/lesson4')
 def lesson4():
     return render_template('lesson4.html', lesson4=lesson4)
 
