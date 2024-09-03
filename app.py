8b43764713eac125f59226bf8523bcd21f35e4c6
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-09-03 11:05:19 +0800

    chore: add news api key for fetching stock tickers

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from app.config import Config
 from app import create_app
-
+import os, warnings
 
 app = create_app(Config)
-
+if not os.environ.get("NEWS_API_KEY"):
+    warnings.warn("NEWS_API_KEY not set")
 if __name__ == '__main__':
     app.run()
--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
+import os
 class Config:
     SECRET_KEY = "Th3W@yT0Financ1alLit3racy"
     DATABASE_NAME='invest.db'
     SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE_NAME
+    NEWS_API_KEY = os.getenv("NEWS_API_KEY")
